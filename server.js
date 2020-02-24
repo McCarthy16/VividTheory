fa2e5347ab7fa0c82bbbc857066902e89e393795
Jordan <jmmccart@ualberta.ca>
2020-02-24 00:35:04 -0700

    changing build folder to dist

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 module.exports = {
   //...
   devServer: {
-    contentBase: path.join(__dirname, 'build'),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3000
   }
