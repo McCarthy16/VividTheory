340ced72d117d93663ed6a72d1b5a62238d484da
Jordan <jmmccart@ualberta.ca>
2020-02-23 22:17:32 -0700

    refactoring folders

---
+++ build/precache-manifest.b468334ecb16e63d40bf7509f0fb5aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353b24baff53fa6cff7a024f738aa33b",
+    "url": "/McCarthy16/VividTheoryindex.html"
+  },
+  {
+    "revision": "b28aefd2fda1251ce6fb",
+    "url": "/McCarthy16/VividTheorystatic/css/main.4807703b.chunk.css"
+  },
+  {
+    "revision": "2f68ab8d4b538e37b18f",
+    "url": "/McCarthy16/VividTheorystatic/js/2.64ec7265.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/McCarthy16/VividTheorystatic/js/2.64ec7265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28aefd2fda1251ce6fb",
+    "url": "/McCarthy16/VividTheorystatic/js/main.1fa65207.chunk.js"
+  },
+  {
+    "revision": "055ad798e744e667dde4",
+    "url": "/McCarthy16/VividTheorystatic/js/runtime-main.36b09c72.js"
+  }
+]);
